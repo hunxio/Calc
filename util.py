6919ad896b39c7c54cccc9fc8f55e44c8a3e6e8c
Huno <136697789+hunxio@users.noreply.github.com>
2024-06-26 21:27:38 +0200

    added basic math functions

--- util.py
+++ util.py
@@ -1,1 +1,16 @@
 # Functions for main.py
+
+def add(x, y):
+    return x + y
+
+
+def substract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
