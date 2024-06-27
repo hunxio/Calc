70b774246790a6853a7790bb25854d5f183d4f98
Huno <136697789+hunxio@users.noreply.github.com>
2024-06-27 23:36:57 +0200

    New button for confirming, renamed window to root

--- main.py
+++ main.py
@@ -4,34 +4,32 @@ from tkinter import ttk
 
 class Window:
     # Initialize tkinter instance
-    def __init__(self, window, widget):
-        self._window = window
+    def __init__(self, root, widget):
+        self._root = root
         self._widget = widget
-        self._frame = ttk.Frame(self._window)
+        self._frame = ttk.Frame(self._root)
 
     # Creating a window object, defining multiple properties
     def configure_window(self):
-        self._window.geometry("600x600")
-        self._window.configure(background="black")
-        self._window.resizable(False, False)
-        self._window.title("Test Title Project")
+        self._root.geometry("600x600")
+        self._root.configure(background="white")
+        self._root.resizable(False, False)
+        self._root.title("Test Title Project")
 
     # Creating widgets to display and interact with inside the window's app
     def create_widgets(self):
-        self._widget.Button(
-            self._window, text="Exit App", command=self._window.destroy
-        ).grid(column=0, row=0)
-
-    @property
-    def frame(self):
-        return self._frame
+        input_bar = self._widget.Entry(self._root, width=20).grid(column=0, row=0)
+        enter_btn = self._widget.Button(self._root, text="Enter").grid(column=1, row=0)
+        quit_btn = self._widget.Button(
+            self._root, text="Exit App", command=self._root.destroy
+        ).grid(column=0, row=1)
 
 
 def main():
-    wind = Window(Tk(), ttk)
-    wind.configure_window()
-    wind.create_widgets()
-    wind._window.mainloop()
+    root = Window(Tk(), ttk)
+    root.configure_window()  # Loads window
+    root.create_widgets()  # Loads widgets
+    root._root.mainloop()  # Mantains the window always open
 
 
 if __name__ == "__main__":
