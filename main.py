23320466b3c65481871af63bf645a112eb4872af
Huno <136697789+hunxio@users.noreply.github.com>
2024-06-27 13:37:03 +0200

    basic layout window

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-import sys
+from tkinter import *
+from tkinter import ttk
 
 def main():
-    ...
+    # Initialize tkinter instance
+    window = Tk() 
+    # Creating a window object, defining the size and if it can be resized
+    frame = ttk.Frame(window)
+    window.geometry("600x600")
+    window.resizable(False, False)
+    window.title("Test Title Project")
+    window.mainloop()
 
 if __name__ == "__main__":
     main()
